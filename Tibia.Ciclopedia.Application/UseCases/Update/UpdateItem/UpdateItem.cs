d0739ee7bd1670ef2992b01b7c1dfb1a57515445
Gabriel Edson Werner Bittencourt <gewbittencourt@hotmail.com>
2024-09-25 12:57:00 -0300

    Refact\ Adicionado tratamento caso o item não seja encontrado.

--- Tibia.Ciclopedia.Application/UseCases/Update/UpdateItem/UpdateItem.cs
+++ Tibia.Ciclopedia.Application/UseCases/Update/UpdateItem/UpdateItem.cs
@@ -24,6 +24,13 @@ namespace Tibia.Ciclopedia.Application.UseCases.UpdateItem.UpdateAllItem
 		public async Task<Output<bool>> Handle(UpdateItemInput request, CancellationToken cancellationToken)
 		{
 			var item = await _itemRepository.GetByIdItems(request.id, cancellationToken);
+			if (item == null)
+			{
+				{
+					return Output<bool>.Failure(new List<string> { "Não foi possível encontrar o item especificado." });
+				}
+			}
+
 			item.UpdateItem(request.Name, request.Type.ToString(), request.Vocations.ToString(), request.Slots, request.Image, request.LevelRequired, request.Price);
 
 			var result = await _itemRepository.UpdateItem(item, cancellationToken);
